ce8a8affa691c6d5f9930f733be187ed0ebdc9b4
PratikshaBhoyar7 <162112323+PratikshaBhoyar7@users.noreply.github.com>
2024-08-06 08:32:06 +0530

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,6 @@
+function cookMaggie(maggie, pani, pan) {
+    console.log("Your maggie will be ready in " + maggie
+     + " minutes with " + pani + " cups of water using a " + pan + " pan.");
+}
+
+cookMaggie(2, 4, 1);
