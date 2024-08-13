cf33a1a5fcc5f1514db0b04cfd539d783b63c16e
Pratiksha B <pratikshabhoyar9966@gmail.com>
2024-08-13 14:52:17 +0530

    array3 updated

--- Array3.js
+++ Array3.js
@@ -6,6 +6,6 @@ for(let i=0; i<Fruit.length-1;i++){
     //console.log(Fruit[i].toUpperCase());
     Fruits2.push(Fruit[i].toUpperCase());
 }
-
+console.log("Here is my new updates");
 console.log(Fruits2);
 console.log(Fruit);
